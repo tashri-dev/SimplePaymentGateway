dfceacdb434c308fe222a589ee53890a24fa8494
tashri <taha.ashri.dv@gmail.com>
2024-11-23 13:53:39 +0200

    feat: omplment domain layer

---
+++ SimplePaymentGateway/src/SimplePaymentGateway.Core/SimplePaymentGateway.Domain/Enums/ResponseCode.cs
@@ -0,0 +1,11 @@
+﻿namespace SimplePaymentGateway.Domain.Enums;
+
+public enum ResponseCode
+{
+    Approved = 0,        // "00"
+    InsufficientFunds = 1, // "01"
+    Rejected = 2,        // "02"
+    Reversed = 3,        // "03"
+    DoNotHonor = 10,     // "10"
+    SystemError = 99     // "99"
+}
---
+++ SimplePaymentGateway/src/SimplePaymentGateway.Core/SimplePaymentGateway.Domain/Exceptions/PaymentGatewayException.cs
@@ -0,0 +1,18 @@
+﻿namespace SimplePaymentGateway.Domain.Exceptions;
+
+public class PaymentGatewayException : Exception
+{
+    public string ErrorCode { get; }
+
+    public PaymentGatewayException(string message, string errorCode = "PAYMENT_ERROR")
+        : base(message)
+    {
+        ErrorCode = errorCode;
+    }
+
+    public PaymentGatewayException(string message, Exception innerException, string errorCode = "PAYMENT_ERROR")
+        : base(message, innerException)
+    {
+        ErrorCode = errorCode;
+    }
+}
