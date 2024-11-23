82fbb68069170d69934c33e99f35d707cb24204f
tashri <taha.ashri.dv@gmail.com>
2024-11-23 14:49:23 +0200

    feat: add contracts for use cases

---
+++ SimplePaymentGateway/src/SimplePaymentGateway.Core/SimplePaymentGateway.Application/Contracts/ITransactionProcessor.cs
@@ -0,0 +1,12 @@
+﻿using SimplePaymentGateway.Application.DTOs;
+using SimplePaymentGateway.Domain.Common;
+using SimplePaymentGateway.Domain.Entities;
+
+namespace SimplePaymentGateway.Application.Contracts;
+
+public interface ITransactionProcessor
+{
+    Task<Result<TransactionResponse>> ProcessTransaction(Transaction transaction);
+    Task<bool> ValidateTransaction(Transaction transaction);
+}
+
